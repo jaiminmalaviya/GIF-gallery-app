8949a33ad7fcd915df47d2d61b609f17f96e89b2
Jaimin Malaviya <Jaiminmalviya56.jm@gmail.com>
2023-10-30 16:21:01 +0530

    Display GIFs on home page and add search bar

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,38 @@
-import Image from 'next/image'
+'use client'
+
+import Feed from '@components/Feed'
+import { useRouter } from 'next/navigation'
+import { useEffect, useState } from 'react'
+import { auth } from '@Firebase'
+import Loading from '@components/Loading'
 
 export default function Home() {
-    return <div></div>
+    const [user, setUser] = useState(null)
+    const [isLoading, setIsLoading] = useState(true)
+    const router = useRouter()
+
+    useEffect(() => {
+        const unsubscribe = auth.onAuthStateChanged((authUser) => {
+            if (authUser) {
+                setUser(authUser)
+            } else {
+                setUser(null)
+                router.push('/registration/login')
+            }
+            setIsLoading(false)
+        })
+        return () => unsubscribe()
+    }, [router])
+
+    if (isLoading) {
+        return <Loading />
+    }
+
+    return (
+        user && (
+            <section>
+                <Feed authUser={user} />
+            </section>
+        )
+    )
 }
---
+++ components/Feed.jsx
@@ -0,0 +1,227 @@
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+import Image from 'next/image'
+import Loading from './Loading'
+import searchIcon from '@public/assets/icons/Search icon.svg'
+import { db } from '@Firebase'
+import { getDoc, doc } from 'firebase/firestore'
+import toast, { Toaster } from 'react-hot-toast'
+
+const Feed = ({ authUser }) => {
+    const [search, setSearch] = useState('')
+    const [prevSearch, setPrevSearch] = useState('')
+    const [gifs, setGifs] = useState([])
+    const [loading, setLoading] = useState(false)
+    const [offset, setOffset] = useState(0)
+    const [currentPage, setCurrentPage] = useState(1)
+    const [favorites, setFavorites] = useState([])
+
+    const limit = 20
+    const totalPages = 10
+
+    const user = authUser
+
+    useEffect(() => {
+        const fetchUserFavorites = async () => {
+            try {
+                const userFavoriteGif = await getDoc(doc(db, 'users', authUser?.uid))
+                setFavorites(userFavoriteGif.data()?.favoriteGif)
+            } catch (error) {
+                toast.error('Something went wrong!', { id: 'error' })
+            }
+        }
+        fetchUserFavorites()
+    }, [])
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                setLoading(true)
+                setPrevSearch(() => search)
+
+                let url = `https://api.giphy.com/v1/gifs/trending?api_key=GlVGYHkr3WSBnllca54iNt0yFbjz7L65&limit=${limit}&offset=${offset}`
+
+                if (search) {
+                    url = `https://api.giphy.com/v1/gifs/search?api_key=GlVGYHkr3WSBnllca54iNt0yFbjz7L65&q=${search}&limit=${limit}&offset=${offset}`
+                }
+                search !== prevSearch && handlePageChange(1)
+
+                const response = await axios.get(url)
+                setGifs(
+                    Array.from(new Set(response.data.data.map((obj) => obj.id))).map((id) => {
+                        return response.data.data.find((obj) => obj.id === id)
+                    })
+                )
+            } catch (error) {
+                toast.error(error)
+            } finally {
+                setLoading(false)
+            }
+        }
+        const source = axios.CancelToken.source()
+        fetchData()
+
+        return () => {
+            source.cancel('Operation canceled by cleanup')
+        }
+    }, [search, offset])
+
+    const handleFavoriteChange = async (gifId) => {
+        try {
+            const res = await fetch(`/api/user/${authUser.uid}?gifId=${gifId}`)
+            if (res.ok) setFavorites(await res.json())
+        } catch (error) {
+            toast.error(error)
+        }
+    }
+
+    const handleSearchChange = (e) => {
+        setSearch(e.target.value)
+    }
+
+    const handlePageChange = (page) => {
+        setOffset(() => (page - 1) * limit)
+        setCurrentPage(page)
+    }
+
+    const getPageNumbers = () => {
+        const pageNumbers = []
+        const maxPages = Math.min(totalPages, 3)
+        let startPage = 1
+
+        if (currentPage > 2 && totalPages > 3) {
+            startPage = Math.min(currentPage - 1, totalPages - 2)
+        }
+
+        for (let i = 0; i < maxPages; i++) {
+            pageNumbers.push(startPage)
+            startPage++
+        }
+
+        return pageNumbers
+    }
+
+    return (
+        <main className="bg-white rounded-[20px] w-full shadow md:mt-0 sm:max-w-2xl md:max-w-3xl lg:max-w-5xl xl:max-w-6xl m-auto p-6 min-h-[72vh] relative mb-10">
+            <div className="flex mb-6 relative">
+                <Image
+                    className="absolute top-[50%] left-4 translate-y-[-50%]"
+                    src={searchIcon.src}
+                    width={20}
+                    height={20}
+                    alt="search icon"
+                ></Image>
+                <input
+                    type="text"
+                    className="w-full bg-slate-100 px-7 py-4 ps-12 mr-4 rounded-xl"
+                    placeholder="Article name or keywords..."
+                    value={search}
+                    onChange={handleSearchChange}
+                />
+                <button className="bg-black px-7 py-4 text-white rounded-xl" value="Search">
+                    Search
+                </button>
+            </div>
+            {loading ? (
+                <Loading />
+            ) : !gifs.length == 0 ? (
+                <div>
+                    <div className="lg:columns-4 md:columns-3 sm:columns-2 columns-1">
+                        {gifs.map((gif) => {
+                            return (
+                                <div key={gif.id} className="relative">
+                                    <img
+                                        className="rounded-md mb-4 w-full hover:"
+                                        src={gif.images.fixed_height.url}
+                                        alt={gif.title}
+                                    />
+                                    {!favorites?.includes(gif.id) ? (
+                                        <div className="absolute bottom-0 left-0 right-0 top-0 h-full w-full overflow-hidden bg-black bg-opacity-50 bg-fixed opacity-0 transition duration-300 ease-in-out hover:opacity-100">
+                                            <button
+                                                className="absolute top-4 right-4"
+                                                onClick={() => handleFavoriteChange(gif.id)}
+                                            >
+                                                <svg
+                                                    xmlns="http://www.w3.org/2000/svg"
+                                                    fill="white"
+                                                    viewBox="0 0 24 24"
+                                                    strokeWidth="1.5"
+                                                    stroke="white"
+                                                    className="w-6 h-6"
+                                                >
+                                                    <path
+                                                        strokeLinecap="round"
+                                                        strokeLinejoin="round"
+                                                        d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
+                                                    />
+                                                </svg>
+                                            </button>
+                                        </div>
+                                    ) : (
+                                        <>
+                                            <div className="absolute bottom-0 left-0 right-0 top-0 h-full w-full overflow-hidden bg-black bg-opacity-50 bg-fixed opacity-0 transition duration-300 ease-in-out hover:opacity-100"></div>
+                                            <button
+                                                className="absolute top-4 right-4"
+                                                onClick={() => handleFavoriteChange(gif.id)}
+                                            >
+                                                <svg
+                                                    xmlns="http://www.w3.org/2000/svg"
+                                                    fill="red"
+                                                    viewBox="0 0 24 24"
+                                                    strokeWidth="1.5"
+                                                    stroke="red"
+                                                    className="w-6 h-6"
+                                                >
+                                                    <path
+                                                        strokeLinecap="round"
+                                                        strokeLinejoin="round"
+                                                        d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
+                                                    />
+                                                </svg>
+                                            </button>
+                                        </>
+                                    )}
+                                </div>
+                            )
+                        })}
+                    </div>
+                    <div className="mt-8 mb-5 flex justify-center gap-5">
+                        <button
+                            className="text-sm font-semibold"
+                            onClick={() => handlePageChange(currentPage - 1)}
+                            disabled={currentPage === 1}
+                        >
+                            Previous
+                        </button>
+                        {getPageNumbers().map((pageNumber) => (
+                            <button
+                                key={pageNumber}
+                                className={`text-sm p-2.5 px-3 ${
+                                    currentPage === pageNumber
+                                        ? 'border-b-4 bg-pink-100 border-b-pink-600'
+                                        : ''
+                                }`}
+                                onClick={() => handlePageChange(pageNumber)}
+                            >
+                                {pageNumber}
+                            </button>
+                        ))}
+                        <button
+                            className="text-sm font-semibold"
+                            onClick={() => handlePageChange(currentPage + 1)}
+                            disabled={currentPage === totalPages}
+                        >
+                            Next
+                        </button>
+                    </div>
+                </div>
+            ) : (
+                <div className="p-5 bg-slate-200 m-auto absolute_center rounded-xl w-max">
+                    No any GIF found 🫡
+                </div>
+            )}
+        </main>
+    )
+}
+
+export default Feed
