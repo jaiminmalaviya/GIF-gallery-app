8b712388c307f87783c4c3a5c94e4a9d231b6b56
Jaimin Malaviya <Jaiminmalviya56.jm@gmail.com>
2023-10-30 15:09:48 +0530

    Loader created

---
+++ components/Loading.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Loading = () => {
+    return (
+        <div className="absolute_center ">
+            <span className="loader"></span>
+        </div>
+    )
+}
+
+export default Loading
