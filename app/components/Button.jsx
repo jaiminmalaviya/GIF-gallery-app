30860a7f1dd9dd846c5c3b79363df5b55a8f5d1e
Jaimin Malaviya <Jaiminmalviya56.jm@gmail.com>
2024-01-10 00:12:36 +0530

    change the bg color of button

--- app/components/Button.jsx
+++ app/components/Button.jsx
@@ -4,7 +4,9 @@ const Button = ({ type, children, disabled }) => {
    return (
       <button
          type={type}
-         className="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+         className={`w-full text-white ${
+            disabled ? 'bg-blue-400' : 'bg-blue-600 hover:bg-blue-700'
+         } focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center`}
          disabled={disabled}
       >
          {children}
